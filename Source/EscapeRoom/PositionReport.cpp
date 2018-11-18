9f369c84c1f501d3f34459372d7b5e93aa769321
Emilijus <slibinas2012@gmail.com>
2018-11-18 15:15:29 +0200

    Doors now opening.

--- Source/EscapeRoom/PositionReport.cpp
+++ Source/EscapeRoom/PositionReport.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReport.h"
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UPositionReport::UPositionReport()
@@ -17,8 +18,9 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-
-	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty!"));
+	FString ObjectName = GetOwner()->GetName();
+	FString ObjectPos = GetOwner()->GetTransform().GetLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *ObjectName, *ObjectPos);
 	
 }
 
